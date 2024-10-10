36a725246e9ee16bfef4ccf4067119448c9f8137
Marcos Aurelio <aureliowozhiak@gmail.com>
2024-10-10 18:14:22 -0300

    aula 03

--- Aula03.java
+++ Aula03.java
@@ -14,6 +14,94 @@
 public class Aula03 {
     public static void main(String[] args) {
         System.out.println("Bem-vindo à Aula 03 - Operadores e expressões");
-        // Código da aula será desenvolvido aqui
+        // O que são operadores?
+        // Operadores são símbolos que representam uma operação que será realizada entre dois valores.
+
+        // Operadores aritméticos: +, -, *, /, %
+        System.out.println("Operadores aritméticos: +, -, *, /, %");
+        //float num1 = 10;
+        //float num2 = 20;
+        //System.out.println("Soma: " + (num1 + num2));
+        //System.out.println("Subtração: " + (num1 - num2));
+        //System.out.println("Multiplicação: " + (num1 * num2));
+        //System.out.println("Divisão: " + (num1 / num2));
+        //System.out.println("Resto da divisão: " + (num1 % num2));
+
+        //float a = 9;
+        //float b = 2;
+        //System.out.println("Divisão: " + (a / b));
+        //System.out.println("Resto da divisão: " + (a % b));
+//
+        //9 / 2 = 4,5
+        //9 % 2 = 1
+//
+        //2 cabe 4 (8) vezes em 9, sobra 1
+
+        //float c = 7;
+        //float d = 3;
+        ////System.out.println("Divisão: " + (c / d));
+        //System.out.println("Resto da divisão: " + (c % d));
+
+        //7 / 3 = 2,3333
+        //7 % 3 = 1
+//
+        //3 cabe 2 vezes (6) em 7, sobra 1
+
+        // Operadores de atribuição: =, +=, -=, *=, /=, %=
+        // = atribuição de valor, ou seja, a variavel recebe o valor do lado direito do operador
+
+
+        // Operadores de comparação: ==, !=, >, <, >=, <=   --> true ou false // ele fala se é verdadeiro ou falso
+        // == é igual
+        // != é diferente
+        // > é maior
+        // < é menor
+        // >= é maior ou igual
+        // <= é menor ou igual
+        
+        //int a = 10;
+        //int b = 10;
+        //System.out.println("a é igual ao b? " + (a == b));
+        //System.out.println("a é diferente de b? " + (a != b));
+        //System.out.println("a é maior que b? " + (a > b));
+        //System.out.println("a é menor que b? " + (a < b));
+        //System.out.println("a é maior ou igual a b? " + (a >= b));
+        //System.out.println("a é menor ou igual a b? " + (a <= b));
+
+
+        // Operadores lógicos: &&, ||, !
+
+        //System.out.println("Operadores lógicos: &&, ||, !");
+        //System.out.println("Tabela verdade do &&, || e !");
+        //System.out.println("&& - AND - Verdadeiro se ambas as condições forem verdadeiras");
+        //System.out.println("|| - OR - Verdadeiro se pelo menos uma das condições for verdadeira");
+        //System.out.println("! - NOT - Nega o valor, se for verdadeiro, vira falso, e se for falso, vira verdadeiro");
+//
+        //boolean verdadeiro = true;
+        //boolean falso = false;
+
+        //System.out.println("verdadeiro && falso: " + (verdadeiro && falso));
+        //System.out.println("verdadeiro || falso: " + (verdadeiro || falso));
+        //System.out.println("!verdadeiro: " + (!verdadeiro));
+        //System.out.println("!falso: " + (!falso));
+
+        //tabela verdade AND
+        //System.out.println("Verdadeiro && Verdadeiro: " + (verdadeiro && verdadeiro));
+        //System.out.println("Verdadeiro && Falso: " + (verdadeiro && falso));
+        //System.out.println("Falso && Verdadeiro: " + (falso && verdadeiro));
+        //System.out.println("Falso && Falso: " + (falso && falso));
+
+        //tabela verdade OR
+        //System.out.println("Verdadeiro || Verdadeiro: " + (verdadeiro || verdadeiro));
+        //System.out.println("Verdadeiro || Falso: " + (verdadeiro || falso));
+        //System.out.println("Falso || Verdadeiro: " + (falso || verdadeiro));
+        //System.out.println("Falso || Falso: " + (falso ||falso));
+
+        // tabela verdade NOT
+        //System.out.println("!verdadeiro: " + (!verdadeiro));
+        //System.out.println("!falso: " + (!falso));
+
+
+        
     }
 }
